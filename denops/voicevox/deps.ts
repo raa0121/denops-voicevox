37bbe91aa022ec60a0b1b3a2a9d9ad94d3ccd761
raa0121 <raa0121@gmail.com>
2021-09-06 00:18:26 +0900

    remove unused ensureArray

--- denops/voicevox/deps.ts
+++ denops/voicevox/deps.ts
@@ -1,6 +1,6 @@
 export { fromFileUrl } from "https://deno.land/std@0.106.0/path/mod.ts";
 export type { Denops } from "https://deno.land/x/denops_std@v1.8.1/mod.ts";
-export { ensureNumber, ensureString, ensureArray } from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
+export { ensureNumber, ensureString } from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
 export { execute } from "https://deno.land/x/denops_std@v1.8.1/helper/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v1.8.1/function/mod.ts";
 export * as helper from "https://deno.land/x/denops_std@v1.8.1/helper/mod.ts";
